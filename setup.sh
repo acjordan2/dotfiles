f38e0411cbc0c3717dec1d3ac51c2b73ee1f1d99
Andrew Jordan <acjordan2@gmail.com>
2016-06-20 20:10:17 -0500

    Better symlink handling

--- setup.sh
+++ setup.sh
@@ -1,9 +1,28 @@
-#!/bin/sh
+#!/bin/bash
 
-ln -s $(pwd)/vim ~/.vim
-ln -s $(pwd)/vimrc ~/.vimrc
+files=("$(pwd)/vim/vim" "$(pwd)/vim/vimrc")
+files_r=(~/.vim ~/.vimrc)
+count=0
+
+for file in "${files_r[@]}"
+do
+    if [ -L $file ]; then
+        rm $file
+        echo "Removing symlink to $file"
+    elif [ -f $file ]; then
+        echo "Backing up existing vim files"
+        if [ -f $file.bak ]; then
+            echo "$file.bak already exists"
+        else
+            echo "Moving $file to $file.bak"
+            mv $file $file.bak
+        fi
+    fi
+    echo "Creating symlink to ${files[$count]}"
+    ln -s ${files[$count]} $file
+    count=$((count+1))
+done;
 
 mkdir ~/.vim/swapfiles
-
 git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 vim +PluginInstall +qall
