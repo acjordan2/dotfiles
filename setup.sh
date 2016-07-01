1b8bd12c72c42ef56dea99726dcb5b4489abd423
Andrew Jordan <acjordan2@gmail.com>
2016-07-01 13:16:43 -0500

    Added setup for powerline fonts

--- setup.sh
+++ setup.sh
@@ -26,3 +26,8 @@ done;
 mkdir ~/.vim/swapfiles
 git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 vim +PluginInstall +qall
+
+mkdir ~/.vim/fonts
+git clone https://github.com/powerline/fonts ~/.vim/fonts
+cd ~/.vim/fonts
+./install.sh
