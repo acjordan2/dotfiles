97b8ce48273be6a9181279ba77e8455cbe42f916
Andrew Jordan <acjordan2@gmail.com>
2017-02-15 15:15:22 -0600

    Changed vundle to plug

--- setup.sh
+++ setup.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 
-git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
-vim +PluginInstall +qall
+source ./setup-symlink.sh
+
+curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
+    https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+vim +PlugInstall
 
 mkdir ~/.vim/fonts
 git clone https://github.com/powerline/fonts ~/.vim/fonts
