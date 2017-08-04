f83e174c3cf2b2abec3786658e60a1d91bc221b1
Andrew Jordan <acjordan2@gmail.com>
2017-08-04 18:23:05 -0500

    Added homebrew package list

---
+++ homebrew.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+
+brew install \
+  binutils \
+  coreutils \
+  ctags \
+  gnu-sed \
+  gnutls \
+  go \
+  grep --with-default-names \
+  nmap \
+  reattach-to-user-namespace
+  tmux \
+  tmux-pasteboard \
+  vim \
+  zsh
