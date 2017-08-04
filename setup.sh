4860c5410f8d6fb95905a0b11c347e8985815cc1
Andrew Jordan <acjordan2@gmail.com>
2017-08-04 18:27:40 -0500

    install ohmyzsh

--- setup.sh
+++ setup.sh
@@ -10,3 +10,5 @@ mkdir ~/.vim/fonts
 git clone https://github.com/powerline/fonts ~/.vim/fonts
 cd ~/.vim/fonts
 ./install.sh
+
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
