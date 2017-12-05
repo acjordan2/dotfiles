68948ae16a3fa835994dfbc37f08a17bcab17f83
Andrew Jordan <acjordan2@gmail.com>
2017-12-05 13:19:33 -0600

    renamted install script

--- install.sh
+++ install.sh
@@ -10,16 +10,20 @@ curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
     https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 vim +PlugInstall
 
+# Install powerline fonts
 mkdir ~/.vim/fonts
 git clone https://github.com/powerline/fonts ~/.vim/fonts
 cd ~/.vim/fonts
 ./install.sh
-# cd ~/.vim/plugged/YouCompleteMe/
-# ./install.py --clang-completer --gocode-completer --racer-completer
 
-uninstall_oh_my_zsh
+# Uninstall oh_my_zsh if it exists
+if [ -n "($type -t uninstall_oh_my_zsh)" ] && [ "$(type -t uninstall_oh_my_zsh)" = function ]; then
+  uninstall_oh_my_zsh
+fi
+
+# Install prezto
 git clone --recursive https://github.com/sorin-ionescu/prezto.git "${ZDOTDIR:-$HOME}/.zprezto"
-
 ln -s "${ZDOTDIR:-$HOME}"/.zprezto/runcoms/.zlogin" ${ZDOTDIR:-$HOME}"/.zlogin
 
+# Symlink customized prezto settings and themes
 find .zprezto -type f -exec ln -s $PWD/{} ~/{} \;
