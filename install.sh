860bb3af1c79f0406bd8902e9b6e83de6a19d4fb
Andrew Jordan <acjordan2@gmail.com>
2019-06-04 18:37:53 -0500

    Iterm2 config

--- install.sh
+++ install.sh
@@ -15,15 +15,3 @@ mkdir ~/.vim/fonts
 git clone https://github.com/powerline/fonts ~/.vim/fonts
 cd ~/.vim/fonts
 ./install.sh
-
-# Uninstall oh_my_zsh if it exists
-if [ -n "($type -t uninstall_oh_my_zsh)" ] && [ "$(type -t uninstall_oh_my_zsh)" = function ]; then
-  uninstall_oh_my_zsh
-fi
-
-# Install prezto
-git clone --recursive https://github.com/sorin-ionescu/prezto.git "${ZDOTDIR:-$HOME}/.zprezto"
-ln -s "${ZDOTDIR:-$HOME}"/.zprezto/runcoms/.zlogin" ${ZDOTDIR:-$HOME}"/.zlogin
-
-# Symlink customized prezto settings and themes
-find .zprezto -type f -exec ln -s $PWD/{} ~/{} \;
