d8781e40e089265da103117e7bdf26cdde582a7f
Andrew Jordan <acjordan2@gmail.com>
2019-06-06 18:13:33 -0500

    Clean up symlink script

--- .gvimrc
+++
@@ -1,5 +0,0 @@
-set guifont=Inconsolata\ for\ Powerline:h15
-source ~/notes/.vim_session
-autocmd VimEnter * cd ~/notes
-autocmd VimEnter * Obsess ~/notes/.vim_session
-set nofoldenable
--- setup-symlink.sh
+++ setup-symlink.sh
@@ -1,25 +1,48 @@
-#!/bin/bash
-
-# this symlinks all the dotfiles (and .vim/) to ~/
-# it also symlinks ~/bin for easy updating
-
+#!/usr/bin/env bash
+# Symlink dotfiles, configs in $XDG_CONFIG_HOME, and a baseline SSH Config
+#
 # this is safe to run multiple times and will prompt you about anything unclear
-
-
-# this is a messy edit of alrra's nice work here:
-#   https://raw.githubusercontent.com/alrra/dotfiles/master/os/create_symbolic_links.sh
-#   it should and needs to be improved to be less of a hack.
-
-
-
-# jump down to line ~140 for the start.
-
-
-
 #
-# utils !!!
-#
+# simplified and cleanup version of alrra's nice work here:
+#   https://github.com/alrra/dotfiles/blob/master/src/os/create_symbolic_links.sh
 
+# Utilities
+print_error() {
+    # Print output in red
+    printf "\e[0;31m  [✖] %s %s\e[0m\n" "${1}" "${2}"
+}
+
+print_info() {
+    # Print output in purple
+    printf "\n\e[0;35m %s\e[0m\n\n" "${1}"
+}
+
+print_question() {
+    # Print output in yellow
+    printf "\e[0;33m  [?] %s\e[0m" "${1}"
+}
+
+print_result() {
+    if  [ "${1}" -eq 0 ]; then
+        print_success "${2}"
+    else
+        print_error "${2}"
+    fi
+    if [ "${3}" = "true" ] && [ "${1}" -ne 0 ]; then
+        exit
+    fi
+}
+
+print_success() {
+    # Print output in green
+    printf "\e[0;32m  [✔] %s\e[0m\n" "${1}"
+}
+
+ask_for_confirmation() {
+    print_question "${1} (y/n) "
+    read -r -n 1
+    printf "\n"
+}
 
 answer_is_yes() {
     [[ "$REPLY" =~ ^[Yy]$ ]] \
@@ -27,203 +50,57 @@ answer_is_yes() {
         || return 1
 }
 
-ask() {
-    print_question "$1"
-    read
-}
-
-ask_for_confirmation() {
-    print_question "$1 (y/n) "
-    read -n 1
-    printf "\n"
-}
-
-ask_for_sudo() {
-
-    # Ask for the administrator password upfront
-    sudo -v
-
-    # Update existing `sudo` time stamp until this script has finished
-    # https://gist.github.com/cowboy/3118588
-    while true; do
-        sudo -n true
-        sleep 60
-        kill -0 "$$" || exit
-    done &> /dev/null &
-
-}
-
-cmd_exists() {
-    [ -x "$(command -v "$1")" ] \
-        && printf 0 \
-        || printf 1
-}
-
 execute() {
-    $1 &> /dev/null
+    ${1} &> /dev/null
     print_result $? "${2:-$1}"
 }
 
-get_answer() {
-    printf "$REPLY"
-}
-
-get_os() {
-
-    declare -r OS_NAME="$(uname -s)"
-    local os=""
-
-    if [ "$OS_NAME" == "Darwin" ]; then
-        os="osx"
-    elif [ "$OS_NAME" == "Linux" ] && [ -e "/etc/lsb-release" ]; then
-        os="ubuntu"
-    fi
-
-    printf "%s" "$os"
-
-}
-
-is_git_repository() {
-    [ "$(git rev-parse &>/dev/null; printf $?)" -eq 0 ] \
-        && return 0 \
-        || return 1
-}
-
-mkd() {
-    if [ -n "$1" ]; then
-        if [ -e "$1" ]; then
-            if [ ! -d "$1" ]; then
-                print_error "$1 - a file with the same name already exists!"
-            else
-                print_success "$1"
-            fi
-        else
-            execute "mkdir -p $1" "$1"
-        fi
-    fi
-}
-
-print_error() {
-    # Print output in red
-    printf "\e[0;31m  [✖] $1 $2\e[0m\n"
-}
-
-print_info() {
-    # Print output in purple
-    printf "\n\e[0;35m $1\e[0m\n\n"
-}
-
-print_question() {
-    # Print output in yellow
-    printf "\e[0;33m  [?] $1\e[0m"
-}
-
-print_result() {
-    [ $1 -eq 0 ] \
-        && print_success "$2" \
-        || print_error "$2"
-
-    [ "$3" == "true" ] && [ $1 -ne 0 ] \
-        && exit
-}
-
-print_success() {
-    # Print output in green
-    printf "\e[0;32m  [✔] $1\e[0m\n"
-}
-
-
-
-
-
-
-#
-# actual symlink stuff
-#
-
-
-# finds all .dotfiles in this folder
-declare -a FILES_TO_SYMLINK=$(find . -type f -maxdepth 1 -name ".*" -not -name .DS_Store -not -name .git -not -name .osx | sed -e 's|//|/|' | sed -e 's|./.|.|')
-FILES_TO_SYMLINK="$FILES_TO_SYMLINK .vim .extra" # add in vim and the binaries
-
-
-# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
-
 main() {
+    local FILES_TO_SYMLINK=()
+    local file
+    local sourceFile
+    local targetFile
 
-    local i=""
-    local sourceFile=""
-    local targetFile=""
+    # dotfiles in $HOME
+    while read -r; do
+        FILES_TO_SYMLINK+=("$(echo "${REPLY}" | sed -e 's|//|/|' | sed -e 's|./.|.|')")
+    done < <(find . -maxdepth 1 -type f -name ".*" -not -name .DS_Store -not -name '.git*' -not -name .macos)
 
-    for i in ${FILES_TO_SYMLINK[@]}; do
-
-        sourceFile="$(pwd)/$i"
-        targetFile="$HOME/$(printf "%s" "$i" | sed "s/.*\/\(.*\)/\1/g")"
-
-        if [ -e "$targetFile" ]; then
-            if [ "$(readlink "$targetFile")" != "$sourceFile" ]; then
-
-                ask_for_confirmation "'$targetFile' already exists, do you want to overwrite it?"
-                if answer_is_yes; then
-                    rm -rf "$targetFile"
-                    execute "ln -fs $sourceFile $targetFile" "$targetFile → $sourceFile"
-                else
-                    print_error "$targetFile → $sourceFile"
-                fi
-
-            else
-                print_success "$targetFile → $sourceFile"
-            fi
-        else
-            execute "ln -fs $sourceFile $targetFile" "$targetFile → $sourceFile"
-        fi
-
+    # config files in $XDG_CONFIG_HOME
+    for dir in .config/*; do
+        FILES_TO_SYMLINK+=("${dir}")
     done
 
-    for i in $(pwd)/.config/*; do
+    # SSH configs only
+    for file in .ssh/*; do
+        if grep -i "PRIVATE KEY" "${file}" -c -q; then 
+            # Obviously don't track private keys via git
+            print_error "SSH private key found in '${file}'! Key should be removed and rotated"
+            return 1
+        fi
+        FILES_TO_SYMLINK+=("${file}")
+    done
 
-        sourceFile="$i"
-        targetFile="$HOME/.config/$(printf "%s" "$i" | sed "s/.*\/\(.*\)/\1/g")"
+    for file in "${FILES_TO_SYMLINK[@]}"; do
+        sourceFile="$(pwd)/${file}"
+        targetFile="$HOME/$(printf "%s" "${file}")" 
 
-        if [ -e "$targetFile" ]; then
-            if [ "$(readlink "$targetFile")" != "$sourceFile" ]; then
-
-                ask_for_confirmation "'$targetFile' already exists, do you want to overwrite it?"
+        if [ -e "${targetFile}" ]; then
+            if [ "$(readlink "${targetFile}")" != "${sourceFile}" ]; then
+                ask_for_confirmation "'${targetFile}' already exists, do you want to overwrite it?"
                 if answer_is_yes; then
-                    rm -rf "$targetFile"
-                    execute "ln -fs $sourceFile $targetFile" "$targetFile → $sourceFile"
+                    rm -rf "${targetFile}"
+                    execute "ln -fs ${sourceFile} ${targetFile}" "${targetFile} → ${sourceFile}"
                 else
-                    print_error "$targetFile → $sourceFile"
+                    print_error "${targetFile} → ${sourceFile}"
                 fi
-
             else
-                print_success "$targetFile → $sourceFile"
+                print_success "${targetFile} → ${sourceFile}"
             fi
-        else
+        else 
             execute "ln -fs $sourceFile $targetFile" "$targetFile → $sourceFile"
         fi
-
     done
-    sourceFile=$(pwd)/.ssh/config
-    targetFile=$HOME/.ssh/config
-
-    if [ -e "$targetFile" ]; then
-        if [ "$(readlink "$targetFile")" != "$sourceFile" ]; then
-            ask_for_confirmation "'$targetFile' already exists, do you want to overwrite it?"
-            if answer_is_yes; then
-                rm -rf "$targetFile"
-                execute "ln -fs $sourceFile $targetFile" "$targetFile → $sourceFile"
-            else
-                print_error "$targetFile → $sourceFile"
-            fi
-
-        else
-            print_success "$targetFile → $sourceFile"
-        fi
-    else
-        execute "ln -fs $sourceFile $targetFile" "$targetFile → $sourceFile"
-    fi
-
 }
 
 main
