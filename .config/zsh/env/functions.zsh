d0a20db283ffb3eae300352ab3dfffa09e49e7aa
Andrew Jordan <acjordan2@gmail.com>
2019-08-26 17:49:53 -0500

    Cache GRC alias generation

--- .config/zsh/env/functions.zsh
+++ .config/zsh/env/functions.zsh
@@ -145,3 +145,80 @@ ql() {
     qlmanage -p "$@" &> /dev/null
   fi
 }
+
+grc-refresh() {
+  if [[ "${TERM}" != dumb ]] && (( ${+commands[grc]} )) ; then
+
+  # Prevent grc aliases from overriding zsh completions.
+    setopt COMPLETE_ALIASES
+
+    # Supported commands
+    cmds=(
+      as \
+      blkid \
+      cc \
+      configure \
+      cvs \
+      df \
+      diff \
+      dig \
+      docker \
+      docker-machine \
+      du \
+      env \
+      fdisk \
+      findmnt \
+      free \
+      g++ \
+      gas \
+      gcc \
+      getsebool \
+      gmake \
+      head \
+      id \
+      ifconfig \
+      ip \
+      iptables \
+      last \
+      ld \
+      ldap \
+      ls \
+      lsblk \
+      lsof \
+      lspci \
+      make \
+      mount \
+      mtr \
+      netstat \
+      nmap \
+      ping \
+      ping6 \
+      ps \
+      semanage \
+      tail \
+      traceroute \
+      traceroute6 \
+      wdiff \
+      whois \
+      iwconfig \
+    );
+  
+    echo "" > "${ZDOTDIR}/env/grc.zsh"
+
+    # Set alias for available commands.
+    for cmd in ${cmds} ; do if (( ${+commands[$cmd]} )) ; then
+        echo "alias ${cmd}=\"grc --colour=auto $(whence ${cmd})\"" >> "${ZDOTDIR}/env/grc.zsh"
+      fi
+    done
+
+    alias colourify="grc -es --colour=auto"
+
+    if [ ${+command[grc]} = "1" ]; then
+      alias sudo='sudo '
+      sudo() { grc sudo "${@}" }
+    fi
+
+    # Clean up variables
+    unset cmds cmd
+  fi
+}
--- .config/zsh/env/grc.zsh
+++ .config/zsh/env/grc.zsh
@@ -1,74 +1,32 @@
-#!/usr/bin/env zsh
 
-if [[ "${TERM}" != dumb ]] && (( ${+commands[grc]} )) ; then
-
-# Prevent grc aliases from overriding zsh completions.
-  setopt COMPLETE_ALIASES
-
-  # Supported commands
-  cmds=(
-    as \
-    blkid \
-    cc \
-    configure \
-    cvs \
-    df \
-    diff \
-    dig \
-    docker \
-    docker-machine \
-    du \
-    env \
-    fdisk \
-    findmnt \
-    free \
-    g++ \
-    gas \
-    gcc \
-    getsebool \
-    gmake \
-    head \
-    id \
-    ifconfig \
-    ip \
-    iptables \
-    last \
-    ld \
-    ldap \
-    ls \
-    lsblk \
-    lsof \
-    lspci \
-    make \
-    mount \
-    mtr \
-    netstat \
-    nmap \
-    ping \
-    ping6 \
-    ps \
-    semanage \
-    tail \
-    traceroute \
-    traceroute6 \
-    wdiff \
-    whois \
-    iwconfig \
-  );
-
-  # Set alias for available commands.
-  for cmd in ${cmds} ; do if (( ${+commands[$cmd]} )) ; then
-      alias ${cmd}="grc --colour=auto $(whence ${cmd})"
-    fi
-  done
-
-  alias colourify="grc -es --colour=auto"
-
-  if [ ${+command[grc]} = "1" ]; then
-    alias sudo='sudo '
-    sudo() { grc sudo "${@}" }
-  fi
-
-  # Clean up variables
-  unset cmds cmd
-fi
+alias as="grc --colour=auto /usr/bin/as"
+alias cc="grc --colour=auto /usr/bin/cc"
+alias df="grc --colour=auto /bin/df"
+alias diff="grc --colour=auto /usr/bin/diff"
+alias dig="grc --colour=auto /usr/bin/dig"
+alias docker="grc --colour=auto /usr/local/bin/docker"
+alias docker-machine="grc --colour=auto /usr/local/bin/docker-machine"
+alias du="grc --colour=auto /usr/bin/du"
+alias env="grc --colour=auto /usr/bin/env"
+alias fdisk="grc --colour=auto /usr/sbin/fdisk"
+alias g++="grc --colour=auto /usr/bin/g++"
+alias gcc="grc --colour=auto /usr/bin/gcc"
+alias head="grc --colour=auto /usr/bin/head"
+alias id="grc --colour=auto /usr/bin/id"
+alias ifconfig="grc --colour=auto /sbin/ifconfig"
+alias last="grc --colour=auto /usr/bin/last"
+alias ld="grc --colour=auto /usr/bin/ld"
+alias ldap="grc --colour=auto /usr/local/bin/ldap"
+alias ls="grc --colour=auto gls -AFh ${colorflag} --group-directories-first"
+alias lsof="grc --colour=auto /usr/sbin/lsof"
+alias make="grc --colour=auto /usr/bin/make"
+alias mount="grc --colour=auto /sbin/mount"
+alias netstat="grc --colour=auto /usr/sbin/netstat"
+alias nmap="grc --colour=auto /usr/local/bin/nmap"
+alias ping="grc --colour=auto /sbin/ping"
+alias ping6="grc --colour=auto /sbin/ping6"
+alias ps="grc --colour=auto /bin/ps"
+alias tail="grc --colour=auto /usr/bin/tail"
+alias traceroute="grc --colour=auto /usr/sbin/traceroute"
+alias traceroute6="grc --colour=auto /usr/sbin/traceroute6"
+alias whois="grc --colour=auto /usr/bin/whois"
