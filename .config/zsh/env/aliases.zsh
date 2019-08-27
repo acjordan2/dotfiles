15dc7a78acbe42d83034f2077afd695c26ab4096
Andrew Jordan <acjordan2@gmail.com>
2019-08-26 22:23:19 -0500

    macOS aliases

--- .config/zsh/env/aliases.zsh
+++ .config/zsh/env/aliases.zsh
@@ -35,9 +35,6 @@ alias sgrep='grep -R -n -H -C 5 --exclude-dir={.git,.svn,CVS} '
 
 ###
 
-# Recursively delete `.DS_Store` files
-alias cleanup_dsstore="find . -name '*.DS_Store' -type f -ls -delete"
-
 alias diskspace_report="df -P -kHl"
 alias free_diskspace_report="diskspace_report"
 
@@ -55,17 +52,9 @@ alias msfc="msfconsole"
 # File size
 alias fs="/usr/bin/stat -f \"%z bytes\""
 
-# Add stuff to Yoink dropbox
-alias yoink="open -a Yoink"  
 
 # Vim scratchpad with autosaving
 alias scratchpad='vim -c "let g:auto_save = 1" ~/scratchpad/`date +"%m_%d_%Y"`.txt'
-
-# System airport utility
-alias airport="/System/Library/PrivateFrameworks/Apple80211.framework/Versions/Current/Resources/airport"
-
-# gnu stty
-alias gstty='/usr/local/opt/coreutils/libexec/gnubin/stty'
 
 # force git pull, overwriting local changes
 alias forcepull='git fetch --all && git reset --hard origin/master'
@@ -79,14 +68,6 @@ alias hist='cat $XDG_DATA_HOME/zsh/.zhistory | cut -d";" -f2 | uniq | head -n -1
 # Show active network interfaces
 alias ifactive="ifconfig | pcregrep -M -o '^[^\t:]+:([^\n]|\n\t)*status: active'"
 
-# Show/hide hidden files in Finder
-alias show="defaults write com.apple.finder AppleShowAllFiles -bool true && killall Finder"
-alias hide="defaults write com.apple.finder AppleShowAllFiles -bool false && killall Finder"
-
-# Hide/show all desktop icons (useful when presenting)
-alias hidedesktop="defaults write com.apple.finder CreateDesktop -bool false && killall Finder"
-alias showdesktop="defaults write com.apple.finder CreateDesktop -bool true && killall Finder"
-
 # URL-encode strings
 alias urlencode='python -c "import sys, urllib as ul; print ul.quote_plus(sys.argv[1]);"'
 
@@ -98,3 +79,26 @@ alias kitty='/Applications/Kitty.app/Contents/MacOS/kitty'
 
 # cd into directories on the stack
 for index ({1..9}) alias "$index"="cd +${index}"; unset index
+
+# macOS aliases
+if [[ "${OSTYPE}" == "darwin"* ]]; then
+  # Recursively delete `.DS_Store` files
+  alias cleanup_dsstore="find . -name '*.DS_Store' -type f -ls -delete"
+
+  # Show/hide hidden files in Finder
+  alias show="defaults write com.apple.finder AppleShowAllFiles -bool true && killall Finder"
+  alias hide="defaults write com.apple.finder AppleShowAllFiles -bool false && killall Finder"
+
+  # Hide/show all desktop icons (useful when presenting)
+  alias hidedesktop="defaults write com.apple.finder CreateDesktop -bool false && killall Finder"
+  alias showdesktop="defaults write com.apple.finder CreateDesktop -bool true && killall Finder"
+
+  # System airport utility
+  alias airport="/System/Library/PrivateFrameworks/Apple80211.framework/Versions/Current/Resources/airport"
+
+  # gnu stty
+  alias gstty='/usr/local/opt/coreutils/libexec/gnubin/stty'
+
+  # Add stuff to Yoink dropbox
+  alias yoink="open -a Yoink"  
+fi
