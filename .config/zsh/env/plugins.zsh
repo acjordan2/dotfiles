f644cfe5ebdd4029183df795b16377802ede0ca7
Andrew Jordan <acjordan2@gmail.com>
2019-09-12 10:17:43 -0500

    clean up

--- .config/zsh/env/plugins.zsh
+++ .config/zsh/env/plugins.zsh
@@ -2,17 +2,17 @@
 
 # ZSH Completoins
 fpath=(
-  $ZDOTDIR/plugins/zsh-completions/src
-  $ZDOTDIR/completion
-  $fpath
+  ${ZDOTDIR}/plugins/zsh-completions/src
+  ${ZDOTDIR}/completion
+  ${fpath}
 )
 
-source "$ZDOTDIR/plugins/zsh-autosuggestions/zsh-autosuggestions.zsh"
-source "$ZDOTDIR/plugins/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh" # Source before zsh-history-substring-search
-source "$ZDOTDIR/plugins/zsh-history-substring-search/zsh-history-substring-search.zsh" # Source last
+source "${ZDOTDIR}/plugins/zsh-autosuggestions/zsh-autosuggestions.zsh"
+source "${ZDOTDIR}/plugins/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh" # Source before zsh-history-substring-search
+source "${ZDOTDIR}/plugins/zsh-history-substring-search/zsh-history-substring-search.zsh" # Source last
 
 # Plugin Options
-#
+
 ## ZSH AutoSuggestions
 ZSH_AUTOSUGGEST_USE_ASYNC=1
 
