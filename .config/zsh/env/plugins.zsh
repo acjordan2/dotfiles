67de6136652f5d83dff7087af077434071f78bae
Andrew Jordan <acjordan2@gmail.com>
2019-09-14 13:02:36 -0500

    Fast syntax highlighting

--- .config/zsh/env/plugins.zsh
+++ .config/zsh/env/plugins.zsh
@@ -1,5 +1,7 @@
 #!/usr/bin/env zsh
 
+HIGHLIGHTER="fsh"
+
 # ZSH Completoins
 fpath=(
   ${ZDOTDIR}/plugins/zsh-completions/src
@@ -7,22 +9,26 @@ fpath=(
   ${fpath}
 )
 
-source "${ZDOTDIR}/plugins/zsh-autosuggestions/zsh-autosuggestions.zsh"
-source "${ZDOTDIR}/plugins/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh" # Source before zsh-history-substring-search
-source "${ZDOTDIR}/plugins/zsh-history-substring-search/zsh-history-substring-search.zsh" # Source last
-
-# Plugin Options
-
 ## ZSH AutoSuggestions
+source "${ZDOTDIR}/plugins/zsh-autosuggestions/zsh-autosuggestions.zsh"
 ZSH_AUTOSUGGEST_USE_ASYNC=1
 
-## ZSH Syntax highlight
-typeset -A ZSH_HIGHLIGHT_STYLES
-ZSH_HIGHLIGHT_HIGHLIGHTERS=(main brackets pattern cursor)
-ZSH_HIGHLIGHT_STYLES[alias]='fg=cyan'
-ZSH_HIGHLIGHT_STYLES[function]='fg=magenta'
-ZSH_HIGHLIGHT_STYLES[builtin]='fg=yellow'
+# easy switch between highlighters if needed
+if [ "${HIGHLIGHTER}" = "fsh" ]; then
+  # Fast syntax highlighting
+  FAST_WORK_DIR="${ZDOTDIR}/plugins/fast-syntax-highlighting-data"
+  source "${ZDOTDIR}/plugins/fast-syntax-highlighting/fast-syntax-highlighting.plugin.zsh"
+else
+  ## ZSH Syntax highlight
+  source "${ZDOTDIR}/plugins/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh" # Source before zsh-history-substring-search
+  typeset -A ZSH_HIGHLIGHT_STYLES
+  ZSH_HIGHLIGHT_HIGHLIGHTERS=(main brackets pattern cursor)
+  ZSH_HIGHLIGHT_STYLES[alias]='fg=cyan'
+  ZSH_HIGHLIGHT_STYLES[function]='fg=magenta'
+  ZSH_HIGHLIGHT_STYLES[builtin]='fg=yellow'
+fi
 
 # ZSH history substring search
+source "${ZDOTDIR}/plugins/zsh-history-substring-search/zsh-history-substring-search.zsh" # Source last
 bindkey '^[[A' history-substring-search-up
 bindkey '^[[B' history-substring-search-down
---
+++ .config/zsh/plugins/fast-syntax-highlighting-data/theme_overlay.zsh
@@ -0,0 +1,4 @@
+: ${FAST_HIGHLIGHT_STYLES[${FAST_THEME_NAME}alias]::=fg=cyan}
+: ${FAST_HIGHLIGHT_STYLES[${FAST_THEME_NAME}builtin]::=fg=yellow}
+: ${FAST_HIGHLIGHT_STYLES[${FAST_THEME_NAME}function]::=fg=magenta}
+: ${FAST_HIGHLIGHT_STYLES[${FAST_THEME_NAME}secondary]::=default}
