167d54c9a44de7617f3e6acb25623d062cfdfbcd
Andrew Jordan <acjordan2@gmail.com>
2019-09-28 11:51:53 +0100

    cleanup

--- .config/zsh/modules/fast-syntax-highlighting/init.zsh
+++ .config/zsh/modules/fast-syntax-highlighting/init.zsh
@@ -1,5 +1,3 @@
 # Fast syntax highlighting
-FAST_WORK_DIR="${ZDOTDIR}/plugins/fast-syntax-highlighting-data"
+FAST_WORK_DIR="${ZDOTDIR}/modules/fast-syntax-highlighting/data"
 source "${ZDOTDIR}/modules/fast-syntax-highlighting/external/fast-syntax-highlighting.plugin.zsh" || return 1 
-
-
