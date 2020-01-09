b45a8112c79bd44f638ae6ca861fb5620ed402cd
Andrew Jordan <acjordan2@gmail.com>
2020-01-09 10:40:13 -0600

    check if fzf exists

--- .config/zsh/modules/fzf/init.zsh
+++ .config/zsh/modules/fzf/init.zsh
@@ -1,2 +1,4 @@
-source /usr/local/opt/fzf/shell/completion.zsh || return 1
-source /usr/local/opt/fzf/shell/key-bindings.zsh || return 1
+if [[ "${+commands[fzf]}" -eq 1 ]]; then
+  source /usr/local/opt/fzf/shell/completion.zsh || return 1
+  source /usr/local/opt/fzf/shell/key-bindings.zsh || return 1
+fi
