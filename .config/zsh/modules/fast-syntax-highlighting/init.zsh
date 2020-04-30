0c20f92b3a858c5df4c56a56f6d5f8f4c1849d38
Andrew Jordan <acjordan2@gmail.com>
2020-04-30 16:05:13 -0500

    typo

--- .config/zsh/modules/fast-syntax-highlighting/init.zsh
+++ .config/zsh/modules/fast-syntax-highlighting/init.zsh
@@ -1,4 +1,4 @@
 # Fast syntax highlighting
 FAST_WORK_DIR="${ZDOTDIR}/modules/fast-syntax-highlighting/data"
 
-[ -d "${ZDOTDIR}/modules/fast-syntax-highlighting/external/fast-syntax-highlighting.plugin.zsh" ] && source "${ZDOTDIR}/modules/fast-syntax-highlighting/external/fast-syntax-highlighting.plugin.zsh"
+[ -f "${ZDOTDIR}/modules/fast-syntax-highlighting/external/fast-syntax-highlighting.plugin.zsh" ] && source "${ZDOTDIR}/modules/fast-syntax-highlighting/external/fast-syntax-highlighting.plugin.zsh"
