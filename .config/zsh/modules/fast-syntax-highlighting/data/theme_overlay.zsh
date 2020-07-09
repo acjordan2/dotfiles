7e860918d2eedf5fa66ee338efe7e64813fd9632
Andrew Jordan <acjordan2@gmail.com>
2020-07-09 12:36:58 -0500

    highlight fix for input redirection

--- .config/zsh/modules/fast-syntax-highlighting/data/theme_overlay.zsh
+++ .config/zsh/modules/fast-syntax-highlighting/data/theme_overlay.zsh
@@ -1,4 +1,5 @@
 : ${FAST_HIGHLIGHT_STYLES[${FAST_THEME_NAME}alias]::=fg=cyan}
 : ${FAST_HIGHLIGHT_STYLES[${FAST_THEME_NAME}builtin]::=fg=yellow}
 : ${FAST_HIGHLIGHT_STYLES[${FAST_THEME_NAME}function]::=fg=magenta}
+: ${FAST_HIGHLIGHT_STYLES[${FAST_THEME_NAME}here-string-text]::=fg=magenta}
 : ${FAST_HIGHLIGHT_STYLES[${FAST_THEME_NAME}secondary]::=default}
